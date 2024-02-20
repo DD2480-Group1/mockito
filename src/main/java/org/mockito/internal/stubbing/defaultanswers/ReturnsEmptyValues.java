2a901b79ae6b4900340e4395718e947896117f1c
Elliot <edarth@kth.se>
2024-02-20 12:27:00 +0100

    Add code to measure coverage of returnValuefor method
    Implement code to store if each branch in the returnValueFor method in ReturnsEmptyValues.java has been reached.
    
    Add code to also print the branch coverage, will use later.
    
    Working #7

--- src/main/java/org/mockito/internal/stubbing/defaultanswers/ReturnsEmptyValues.java
+++ src/main/java/org/mockito/internal/stubbing/defaultanswers/ReturnsEmptyValues.java
@@ -103,57 +103,104 @@ public class ReturnsEmptyValues implements Answer<Object>, Serializable {
         return returnValueFor(returnType);
     }
 
+    private static final int BRANCH_COUNT = 23;
+    private static boolean branches[] = new boolean[BRANCH_COUNT];
+    private static final String FILE_NAME = "[ReturnsEmptyValues.java]";
+    private static final String FUNCTION_NAME = "[@returnValueFor]";
+
+    public static void reachBranch(int id) {
+        branches[id] = true;
+    }
+    public static void printBranchCoverage() {
+        int coverage = 0;
+        String state = "";
+        for (int i = 0; i < branches.length; i++) {
+            if (branches[i]) {
+                state = "True";
+                coverage++;
+            } else {
+                state = "False";
+            }
+
+            System.out.println(FILE_NAME + " " + FUNCTION_NAME + " Branch " + i + ": " + state);
+        }
+        double percentile = ((double) coverage / BRANCH_COUNT) * 100;
+        System.out.println(FILE_NAME + " " + FUNCTION_NAME + " Branch hits: " + coverage + " (" + percentile + "%)");
+    }
+
     Object returnValueFor(Class<?> type) {
         if (Primitives.isPrimitiveOrWrapper(type)) {
+            reachBranch(0);
             return Primitives.defaultValue(type);
             // new instances are used instead of Collections.emptyList(), etc.
             // to avoid UnsupportedOperationException if code under test modifies returned
             // collection
         } else if (type == Iterable.class) {
+            reachBranch(1);
             return new ArrayList<>(0);
         } else if (type == Collection.class) {
+            reachBranch(2);
             return new LinkedList<>();
         } else if (type == Set.class) {
+            reachBranch(3);
             return new HashSet<>();
         } else if (type == HashSet.class) {
+            reachBranch(4);
             return new HashSet<>();
         } else if (type == SortedSet.class) {
+            reachBranch(5);
             return new TreeSet<>();
         } else if (type == TreeSet.class) {
+            reachBranch(6);
             return new TreeSet<>();
         } else if (type == LinkedHashSet.class) {
+            reachBranch(7);
             return new LinkedHashSet<>();
         } else if (type == List.class) {
+            reachBranch(8);
             return new LinkedList<>();
         } else if (type == LinkedList.class) {
+            reachBranch(9);
             return new LinkedList<>();
         } else if (type == ArrayList.class) {
+            reachBranch(10);
             return new ArrayList<>();
         } else if (type == Map.class) {
+            reachBranch(11);
             return new HashMap<>();
         } else if (type == HashMap.class) {
+            reachBranch(12);
             return new HashMap<>();
         } else if (type == SortedMap.class) {
+            reachBranch(13);
             return new TreeMap<>();
         } else if (type == TreeMap.class) {
+            reachBranch(14);
             return new TreeMap<>();
         } else if (type == LinkedHashMap.class) {
+            reachBranch(15);
             return new LinkedHashMap<>();
         } else if (type == Stream.class) {
+            reachBranch(16);
             return Stream.empty();
         } else if (type == DoubleStream.class) {
+            reachBranch(17);
             return DoubleStream.empty();
         } else if (type == IntStream.class) {
+            reachBranch(18);
             return IntStream.empty();
         } else if (type == LongStream.class) {
+            reachBranch(19);
             return LongStream.empty();
         } else if (type == Duration.class) {
+            reachBranch(20);
             return Duration.ZERO;
         } else if (type == Period.class) {
+            reachBranch(21);
             return Period.ZERO;
         }
         // Let's not care about the rest of collections.
-
+        reachBranch(22);
         return returnCommonEmptyValueFor(type);
     }
 
