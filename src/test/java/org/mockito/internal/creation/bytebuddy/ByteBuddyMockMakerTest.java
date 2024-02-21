42a0523da539869773be4ddd756da3b2ae4a9060
ZOU Hetai <33616271+JXNCTED@users.noreply.github.com>
2024-02-21 20:49:15 +0100

    Test: Add and finished testcases for ByteBuddyMockMakerTest
    - Add 3 testcases for ByteBuddyMockMakerTest
    - Write comment to explain the purpose of each testcase
    
    Working on #17

--- src/test/java/org/mockito/internal/creation/bytebuddy/ByteBuddyMockMakerTest.java
+++ src/test/java/org/mockito/internal/creation/bytebuddy/ByteBuddyMockMakerTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.internal.creation.settings.CreationSettings;
 import org.mockito.internal.handler.MockHandlerImpl;
+import org.mockito.plugins.MockMaker;
 import org.mockitoutil.TestBase;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,6 +18,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ByteBuddyMockMakerTest extends TestBase {
 
     @Mock private SubclassByteBuddyMockMaker delegate;
+
+    @Mock private SubclassByteBuddyMockMaker delegateStatic;
 
     @Test
     public void should_delegate_call() {
@@ -39,15 +42,65 @@ public class ByteBuddyMockMakerTest extends TestBase {
     }
 
     @Test
-    public void simple_mock_maker() {
+    public void simple_mock_maker_not_delegate() {
+        // create a mock class mockMaker with no delegate
         ByteBuddyMockMaker mockMaker = new ByteBuddyMockMaker();
+        // ensure the mockMaker is not null
         assertThat(mockMaker).isNotNull();
     }
 
     @Test
-    public void test_clear_cache() {
-        ByteBuddyMockMaker mockMaker = new ByteBuddyMockMaker();
-        mockMaker.clearAllCaches();
+    public void test_clear_mock_cache() {
+        ByteBuddyMockMaker mockMaker = new ByteBuddyMockMaker(delegate);
+        CreationSettings<Base> creationSettings = new CreationSettings<Base>();
+        MockHandlerImpl<Base> handler = new MockHandlerImpl<Base>(creationSettings);
         assertThat(mockMaker).isNotNull();
+
+        MockMaker.StaticMockControl<Base> control = mockMaker.createStaticMock(Base.class, creationSettings, handler);
+        mockMaker.createMockType(creationSettings);
+        mockMaker.getHandler(this);
+        mockMaker.isTypeMockable(Base.class);
+        mockMaker.resetMock(this, handler, creationSettings);
+        // the static mock is not supported by ByteBuddyMockMaker and thus
+        // will return null
+        assertThat(control).isNull();
+
+        // clear all caches
+        mockMaker.clearAllCaches();
+
+        // verify all calls to the mockMaker
+        // dogfooding, use Mockito to verify the calls to the mockMaker
+        verify(delegate).createStaticMock(Base.class, creationSettings, handler);
+        verify(delegate).createMockType(creationSettings);
+        verify(delegate).getHandler(this);
+        verify(delegate).isTypeMockable(Base.class);
+        verify(delegate).resetMock(this, handler, creationSettings);
+        // verify cache is cleared
+        verify(delegate).clearAllCaches();
     }
+
+    @Test
+    public void test_static_mock_should_delegate_all() {
+        // create a mock class mockMaker
+        ByteBuddyMockMaker mockMaker = new ByteBuddyMockMaker(delegateStatic);
+        CreationSettings<Object> creationSettings = new CreationSettings<Object>();
+        MockHandlerImpl<Object> handler = new MockHandlerImpl<Object>(creationSettings);
+
+        // delegate all calls to the mockMaker with static mockMaker
+        mockMaker.createStaticMock(Object.class, creationSettings, handler);
+        mockMaker.createMockType(creationSettings);
+        mockMaker.getHandler(this);
+        mockMaker.isTypeMockable(Object.class);
+        mockMaker.resetMock(this, handler, creationSettings);
+
+        // verify all calls to the mockMaker with static mockMaker
+        // dogfooding, use Mockito to verify the calls to the mockMaker
+        verify(delegateStatic).createStaticMock(Object.class, creationSettings, handler);
+        verify(delegateStatic).createMockType(creationSettings);
+        verify(delegateStatic).getHandler(this);
+        verify(delegateStatic).isTypeMockable(Object.class);
+        verify(delegateStatic).resetMock(this, handler, creationSettings);
+    }
+
+    abstract static class Base {}
 }
