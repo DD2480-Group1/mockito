d68224546a7940246c313d503bc1a108d9cfaa5a
Elliot <18326767+real-darth@users.noreply.github.com>
2024-02-21 22:34:04 +0100

    Add documentation for SerializableMethod equals unit tests
    Add documentation to all my unit tests implemented for the `equals` method for `SerializableMethod`. Explains in more detail what they cover.
    
    Working #18

--- src/test/java/org/mockito/internal/invocation/SerializableMethodTest.java
+++ src/test/java/org/mockito/internal/invocation/SerializableMethodTest.java
@@ -73,11 +73,24 @@ public class SerializableMethodTest extends TestBase {
     }
 
     // TODO: add tests for generated equals() method
+
+    /**
+     * Ensure that if we try to compare to null, it returns false.
+     * expected: returns false
+     * @throws Exception
+     */
     @Test
     public void shouldNotBeEqualForNull() throws Exception {
         assertFalse(method.equals(null));
     }
 
+    /**
+     * Tests if we compare two methods with same classes (String), but with different method names, the equals should
+     * return False.
+     * Will therefore cover specific branch where two method names are not the same.
+     * expected: toString and hashCode are two different method names, therefore should return false
+     * @throws Exception
+     */
     @Test
     public void shouldNotBeEqualForDifferentMethodNames() throws Exception {
         Method toStringMethod = String.class.getMethod("toString", args);
@@ -89,11 +102,23 @@ public class SerializableMethodTest extends TestBase {
         assertFalse(methodToString.equals(methodHashCode));
     }
 
+    /**
+     * Test if we compare between different class types.
+     * Object Class is different from String Class.
+     * expected: the classes are different so should return false
+     * @throws Exception
+     */
     @Test
     public void shouldNotBeEqualForDifferentClassTypes() throws Exception {
         assertFalse(method.equals(new Object()));
     }
 
+    /**
+     * Test if we compare between two methods that have the same class, same method name, but different parameterTypes.
+     * StringBuilder "append" method can be overloaded, so it can take different inputs parameters.
+     * expected: The parameterTypes for the two append methods are different, so should return false
+     * @throws Exception
+     */
     @Test
     public void shouldNotBeEqualForDifferentParameterTypes() throws Exception {
         Method appendWithInt = StringBuilder.class.getMethod("append", int.class);
@@ -105,6 +130,13 @@ public class SerializableMethodTest extends TestBase {
         assertFalse(method1.equals(method2));
     }
 
+    /**
+     * Using Mockito's mock class to specifically test for if getDeclaringClass() returns null, by setting that in mock.
+     * This covers a specific branch case if the object that is comparing has getDeclaringClass return null while
+     * the comparison object's getDeclaringClass does not return null.
+     * expected: equals should return false
+     * @throws Exception
+     */
     @Test
     public void shouldNotBeEqualWhenDeclaringClassIsNull() throws Exception {
         // given
@@ -120,6 +152,12 @@ public class SerializableMethodTest extends TestBase {
         assertFalse(mockSerializableMethod.equals(method));
     }
 
+    /**
+     * This test check if equals method can handle when method name of invoking object is null.
+     * Constructed using mock, create a mock method where the getGanme() returns null.
+     * expected: Method name of invoking object should be null, but comparison object is not null, thus return false
+     * @throws Exception
+     */
     @Test
     public void shouldNotBeEqualWhenMethodNameIsNull() throws Exception {
         // given
@@ -135,6 +173,13 @@ public class SerializableMethodTest extends TestBase {
         assertFalse(mockSerializableMethod.equals(method));
     }
 
+    /**
+     * This test covers a case where the returnType of a method is null. A mock method is created where the
+     * getReturnType() method is set to return null. When mock method is invoked for equals, it should fail test where
+     * invoking object returnType is null but comparison objects returnType is not null.
+     * expected: invoking objects returnType is null and comparison object is not null, thus return false
+     * @throws Exception
+     */
     @Test
     public void shouldNotBeEqualWhenReturnTypeIsNull() throws Exception {
         // given
@@ -151,6 +196,12 @@ public class SerializableMethodTest extends TestBase {
         assertFalse(mockSerializableMethod.equals(method));
     }
 
+    /**
+     * Test whether equals can handel when invoking object and comparison object have same parameters for nearly
+     * everything except the returnTypes. Using mock to construct mock method to test against.
+     * expected: since method and mockMethod have different returnTypes, equals should return false
+     * @throws Exception
+     */
     @Test
     public void shouldNotBeEqualForDifferentReturnTypes() throws Exception {
         // given
